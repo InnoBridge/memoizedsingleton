c83515567337985496f831ded9119cfbaca0a3a0
Yi leng Yao <yilengyao@gmail.com>
2025-10-16 14:31:54 -0700

    add config singleton

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as component from '@/building-blocks/component';
 import { RequestMetadata } from '@/components/request_metadata';
 import { UserMetadata } from '@/components/user_metadata';
 import { Authentication, AuthenticationMethod } from '@/components/authentication';
+import { Config, getConfig } from '@/components/config';
 
 export {
     scopes,
@@ -10,5 +11,7 @@ export {
     RequestMetadata,
     UserMetadata,
     Authentication,
-    AuthenticationMethod
+    AuthenticationMethod,
+    Config,
+    getConfig
 };
